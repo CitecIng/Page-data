53a8d379dc4bd6166b3759342978305dde2a4b44
CitecIng <citecingsistemas@gmail.com>
2025-02-06 09:46:35 -0600

    Respaldo de datos a dia de hoy

---
+++ Dinamic/Contenct.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function loadContent(section) {
+      fetch(`/Pagina/${section}.html`)
+          .then((response) => response.text())
+          .then((data) => {
+              document.getElementById("main-content").innerHTML = data;
+              console.log(`Se ha cargado la sección: ${section}`);
+          })
+          .catch((error) => {
+              console.error("Error al cargar la sección:", error);
+          });
+  }
+
+  // Agregar eventos a los enlaces de navegación
+  document.querySelectorAll("nav a").forEach((link) => {
+      link.addEventListener("click", (event) => {
+          event.preventDefault(); // Evita la recarga de la página
+          const section = event.target.getAttribute("data-section");
+          loadContent(section);
+      });
+  });
+
+  // Cargar la sección inicial
+  loadContent("Inicio");
+});
